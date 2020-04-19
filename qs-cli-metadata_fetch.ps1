4ffc9bc803f6a643661e39f31b1bf880cd77219f
Levi Turner <levi.turner+github@gmail.com>
2020-04-18 21:23:21 -0400

    Delete qs-cli-metadata_fetch.ps1

--- qs-cli-metadata_fetch.ps1
+++
@@ -1,6 +0,0 @@
-#Requires -Modules Qlik-Cli
-$Data = Get-Content C:\ProgramData\Qlik\Sense\Host.cfg
-$FQDN = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($($Data)))
-Connect-Qlik -ComputerName https://$($FQDN):443 -UseDefaultCredentials
-$opsmon = Get-QlikApp -filter "name eq 'Operations Monitor'"
-Invoke-QlikGet -path https://$($FQDN)/api/v1/apps/$($opsmon.id)/data/metadata
