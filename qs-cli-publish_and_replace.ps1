27f7e78a91ff7bbf6a1b956221bd48e664e06fdf
Levi Turner <levi.turner+github@gmail.com>
2020-04-18 21:24:05 -0400

    Delete qs-cli-publish_and_replace.ps1

--- qs-cli-publish_and_replace.ps1
+++
@@ -1,5 +0,0 @@
-#Requires -Modules Qlik-Cli
-Connect-Qlik
-$new = Get-QlikApp -filter "name eq 'Operations Monitor(1)'"
-$old = Get-QlikApp -filter "name eq 'Operations Monitor'"
-Invoke-QlikPut -path /qrs/app/$($new.id)/replace?app=$($old.id)
