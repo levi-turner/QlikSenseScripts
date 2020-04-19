7538786df58d0e3563718787cf7fc48dafc0e4c1
Levi Turner <levi.turner+github@gmail.com>
2020-04-18 21:22:06 -0400

    Delete qs-cli-delete_old_apps.ps1

--- qs-cli-delete_old_apps.ps1
+++
@@ -1,5 +0,0 @@
-#Requires -Modules Qlik-Cli
-Connect-Qlik
-$InactivityThreshold=7
-$createDate = (Get-Date).AddDays(-$InactivityThreshold).GetDateTimeFormats()[45]
-Get-QlikApp -filter "createdDate lt '$createDate'" | Remove-QlikApp
