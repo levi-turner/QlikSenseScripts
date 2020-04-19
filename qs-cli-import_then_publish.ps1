bf2f25b5a1af9b945ac2f49ad8681890f89da660
Levi Turner <levi.turner+github@gmail.com>
2020-04-18 21:23:05 -0400

    Delete qs-cli-import_then_publish.ps1

--- qs-cli-import_then_publish.ps1
+++
@@ -1,8 +0,0 @@
-#Requires -Modules Qlik-Cli
-Connect-Qlik
-Set-Location C:\Temp
-$app = Import-QlikApp -file "exampleApp2.qvf" -name "Testing" -upload 
-Write-Host "App $($app.name) with ID $($app.id) uploaded"
-$stream = Get-QlikStream -filter "name eq 'Everyone'"
-Write-Host "Stream $($stream.name) with ID $($stream.id) exists"
-$app | Publish-QlikApp -stream Everyone
